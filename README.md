13caae813d0bab777904c8463a55997d1028e5a1
Benjamin Schubert <bschubert@ca.ibm.com>
2019-11-18 12:15:57 -0500

    Add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# sound-switch-applet
+
+A Cinnamon Applet for switching sound output devices.
+
+The motivation for this project and the development process are described [in this blog post](https://benjuan26.com/blog/writing-a-simple-task-applet-for-cinnamon-desktop/).
--- scripts/switch-audio.sh
+++ scripts/switch-audio.sh
@@ -5,9 +5,7 @@ KEY=device
 CURRENT_DEVICE=$(gsettings get ${SCHEMA} ${KEY})
 
 if [ "$CURRENT_DEVICE" = "'analog-output-lineout'" ]; then
-	echo "first one"
 	gsettings set ${SCHEMA} ${KEY} "analog-output-headphones"
 else
-	echo "second one"
 	gsettings set ${SCHEMA} ${KEY} "analog-output-lineout"
 fi
